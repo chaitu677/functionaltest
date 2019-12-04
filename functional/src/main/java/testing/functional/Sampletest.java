3710f93c23ff448a2fa0548c16883c2803e142a5
Purna <chaitu677@gmail.com>
2019-12-04 20:51:07 +0530

    Saple Push

---
+++ functional/src/main/java/testing/functional/Sampletest.java
@@ -0,0 +1,11 @@
+package testing.functional;
+
+public class Sampletest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to my Program");
+
+	}
+
+}
